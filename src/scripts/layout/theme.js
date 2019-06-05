f2f4d09cb4276e4225cbddef3bb048d4cb276d47
ejf89 <eric.j.farber@gmail.com>
2019-06-04 22:26:12 -0400

    sign;

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -20,6 +20,10 @@ $(document).ready(function(){
 
       // tools.barbaInit()
       $('.hero-static').addClass('load')
+      console.log('NO JS LOAD');
+      console.log('NO JS LOAD');
+      console.log('NO JS LOAD');
+      console.log('NO JS LOAD');
       $('.product-section-block').trigger('click');
 
       tools.letterCLass()
